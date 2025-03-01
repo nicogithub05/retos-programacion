a2e07cc1670d7d1465d29be8522c5e1354999be9
nivamol <nicolasgithub@gmail.com>
2025-03-01 14:44:23 -0500

    correcion1

--- ejercicios/eje2.js
+++ ejercicios/eje2.js
@@ -42,34 +42,34 @@ my_number = 11  // asignación
 print(my_number)
 my_number += 1  // suma y asignación
 print(my_number)
-my_number -= 1  # resta y asignación
+my_number -= 1  // resta y asignación
 print(my_number)
-my_number *= 2  # multiplicación y asignación
+my_number *= 2  // multiplicación y asignación
 print(my_number)
-my_number /= 2  # división y asignación
+my_number /= 2  // división y asignación
 print(my_number)
-my_number %= 2  # módulo y asignación
+my_number %= 2  // módulo y asignación
 print(my_number)
-my_number **= 1  # exponente y asignación
+my_number **= 1  // exponente y asignación
 print(my_number)
-my_number //= 1  # división entera y asignación
+my_number //= 1  // división entera y asignación
 print(my_number)
 
-# Operadores de identidad
+// Operadores de identidad
 my_new_number = my_number
-print(f"my_number is my_new_number es {my_number is my_new_number}")
-print(f"my_number is not my_new_number es {my_number is not my_new_number}")
+print("my_number is my_new_number es {my_number is my_new_number}")
+print("my_number is not my_new_number es {my_number is not my_new_number}")
 
-# Operadores de pertenencia
-print(f"'u' in 'mouredev' = {'u' in 'mouredev'}")
-print(f"'b' not in 'mouredev' = {'b' not in 'mouredev'}")
+//  Operadores de pertenencia
+print("'u' in 'mouredev' = {'u' in 'mouredev'}")
+print("'b' not in 'mouredev' = {'b' not in 'mouredev'}")
 
-# Operadores de bit
-a = 10  # 1010
-b = 3  # 0011
-print(f"AND: 10 & 3 = {10 & 3}")  # 0010
-print(f"OR: 10 | 3 = {10 | 3}")  # 1011
-print(f"XOR: 10 ^ 3 = {10 ^ 3}")  # 1001
-print(f"NOT: ~10 = {~10}")
-print(f"Desplazamiento a la derecha: 10 >> 2 = {10 >> 2}")  # 0010
-print(f"Desplazamiento a la izquierda: 10 << 2 = {10 << 2}")  # 101000
+// Operadores de bit
+a = 10  // 1010
+b = 3  // 0011
+print("AND: 10 & 3 = {10 & 3}")  // 0010
+print("OR: 10 | 3 = {10 | 3}")  // 1011
+print("XOR: 10 ^ 3 = {10 ^ 3}")  // 1001
+print("NOT: ~10 = {~10}")
+print("Desplazamiento a la derecha: 10 >> 2 = {10 >> 2}")  // 0010
+print("Desplazamiento a la izquierda: 10 << 2 = {10 << 2}")  // 101000
