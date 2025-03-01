e13aedafe40b75c93f624af2e51729eb91f1dc6f
nivamol <nicolasgithub@gmail.com>
2025-03-01 14:27:22 -0500

    ejercicio 1

---
+++ ejercicios/eje1.js
@@ -0,0 +1,18 @@
+// https://www.javascript.com/
+
+// comentario de una linea
+/* 
+ Comentario de varias lineas
+*/
+
+let nombre = 'Nicolás'; // variable
+const apellido = 'Valencia'; // constante
+
+let str = 'String';
+let bool = true || false;
+let num = 10;
+let bigIntr = BigInt; // trabajar con números enteros de precisión arbitraria 
+let simbolo = Symbol; // se utiliza como clave en objetos
+let undf = undefined; // valor no definido
+
+console.log('Hola Javascript')
